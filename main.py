4b9c03927b6789e309e556ef51902d1f0989d9b2
Vlad Usatii <vladusatii@gmail.com>
2021-02-28 12:01:15 -0600

    added 2d rendering

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-#!python3
+#!/usr/bin/env python3
+# (execute in terminal with	`chmod +x main.py` and `./main.py`)
+#
+# TinySort
+#
+# Extremely lightweight modeling software, used for production at readproduct.com/careers
+#
+# //////////////////////////////////////////////////////////////////////////// #
+#
+# Created by Vlad Usatii in 2 Hours
+#
+# Full Open-Source Code
+# MIT License
+# Product
+#
+# //////////////////////////////////////////////////////////////////////////// #
+
+# qt5
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5.uic import *
 
+# sys tools
 import sys, os
 import webbrowser
+
+# pil
+from PIL import Image, ImageColor
+
+# ml experimenting
+import cv2
+
+# math
+import math
+import random
+import numpy as np
+import scipy.misc as smp
+import pandas as pd
+
+# sdl2
+import sdl2
+import sdl2.ext
+
+import subprocess
 
 class Screen(QMainWindow):
 	def __init__(self):
@@ -23,10 +61,6 @@ class Screen(QMainWindow):
 			self.lbl.setGeometry(X, Y, posX, posY)
 			self.lbl.setAlignment(Qt.AlignCenter)
 
-		def imp():
-			print("Yeet")
-
-
 		# important text and buttons
 		label(text="tinysort", style="color: rgb(255, 255, 255);font:32pt 'Proxima Nova';", X=0, Y=1080//6, posX=1920//2, posY=1080//16)
 
@@ -35,11 +69,19 @@ class Screen(QMainWindow):
 		self.quitBtn.setToolTip('Quit')
 		self.quitBtn.setGeometry(0, 0, 50, 50)
 		self.quitBtn.clicked.connect(self.close)
+
+		self.needData = QPushButton('Confused about data? Read this.', self)
+		self.needData.setStyleSheet("background-color: transparent;color: rgb(31, 128, 255);")
+		self.needData.setToolTip("You will be taken to our documentation PDF.")
+		self.needData.setGeometry(1920//5.75, 1080//2.5, 300, 50)
+		self.needData.clicked.connect(lambda: self.learnAboutData())
+
 	
-		self.importBtn = QPushButton('Import Data (.csv)', self)
+		self.importBtn = QPushButton('Open Editor', self)
 		self.importBtn.setStyleSheet("background-color: transparent;border:2px solid white;color: rgb(255, 255, 255);padding: 5px;")
-		self.importBtn.setToolTip('Import data in the form of a csv file, only. This is so that we can read your data in the correct formatting.')
+		self.importBtn.setToolTip('Open the 2D and 3D editor before importing your .csv file.')
 		self.importBtn.setGeometry(1920//4 - 210, 1080//4, 200, 50)
+		self.importBtn.clicked.connect(lambda: self.openEditor())
 
 		self.openSourceBtn = QPushButton('Fork on GitHub', self)
 		self.openSourceBtn.setStyleSheet("background-color: black;border:2px solid black;color: rgb(255, 255, 255);padding: 5px;")
@@ -49,11 +91,29 @@ class Screen(QMainWindow):
 
 
 		# credits text
-		label(text="Copyright © 2020 readproduct.com. All rights reserved.", style="color: rgb(150, 150, 150);font:14pt 'Proxima Nova';", X=0, Y=1080//2.25, posX=1920//2, posY=1080//16)
+		label(text="Open Source 2021 by readproduct.com. Created by Vlad Usatii.", style="color: rgb(150, 150, 150);font:14pt 'Proxima Nova';", X=0, Y=1080//2.25, posX=1920//2, posY=1080//16)
 		
 
 		self.oldPos = self.pos()
 		self.show()
+
+	def learnAboutData(self):
+		# dialog process
+		file = "/Users/vladusatii/Dev/sorting/documentation.pdf"
+		webbrowser.open_new(file)
+
+	def openEditor(self):
+		dialog = QFileDialog()
+		dialog.setFileMode(QFileDialog.AnyFile)
+		dialog.setFilter(QDir.Files)
+		if dialog.exec_():
+			file_name = dialog.selectedFiles()
+			if file_name[0] != "":
+				with open(file_name[0], 'r') as f:
+					self.useVc(f.read(), f.name)
+			else:
+				sys.exit(0)
+		self.close()
 
 	def center(self):
 		qr = self.frameGeometry()
@@ -69,11 +129,58 @@ class Screen(QMainWindow):
 		self.move(self.x() + delta.x(), self.y() + delta.y())
 		self.oldPos = event.globalPos()
 
-app = QApplication(sys.argv)
-app.setStyleSheet("QMainWindow{background-color: #121212;border: 0px solid white;}")
 
-screen = Screen()
+	def useVc(self, coords, filename):
+		ew = EditingWindow(filename)
+		data = np.zeros((1920//2, 1080//2, 3), dtype=np.uint8)
+		"""
+		FILE STRUCTURE:
+		X   Y   COLOR R   COLOR G   COLOR B
+		X   Y   COLOR R   COLOR G   COLOR B
+		... ...          ... ... ...
+		"""
+		coord_all = []
+
+		coord_all.append(list(map(int, coords.split())))
+
+		print(coord_all)
+
+		while True:
+			for line in coord_all:
+					data[line[0]:line[0]+2, line[1]:line[1]+2] = [line[2], line[3], line[4]] # add as many as needed
+					ew.blit(data.swapaxes(0, 1))
+	
+			# update page with changes
+			updates = sdl2.ext.get_events()
+			for event in updates:
+				if event.type == sdl2.SDL_KEYDOWN:
+					data[:, :] = [0, 0, 0]
+					xp += n
+					yp += n
+					data[x+n:x+xp, y+n:y+yp] = [255,0,0] # add as many as needed
+					n += 10
+				elif event.type == sdl2.SDL_QUIT:
+					sys.exit(0)
+
+
+# base blitting template
+class EditingWindow(object):
+	def __init__(self, name):
+		sdl2.ext.init()
+		self.surf = sdl2.ext.Window(str(os.environ.get("USER")) + "\'s 2D Model --  " + str(name), size=(1920//2, 1080//2))
+		self.surf.show()
+
+	def blit(self, vc):
+		win = sdl2.ext.pixels3d(self.surf.get_surface())
+		win[:, :, 0:3] = vc.swapaxes(0, 1)
+		self.surf.refresh()
+
+
+
+app = QApplication(sys.argv)
+app.setStyleSheet("QMainWindow{background-color: #121212}")
 
 if __name__ == '__main__':
+	running = False
 	Screen()
 	sys.exit(app.exec_())
